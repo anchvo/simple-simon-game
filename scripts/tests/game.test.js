97a90a7cb27ee5a3b68fa6c946a6c0d8654cfe5e
Anne Voretzsch <ac.voretzsch@gmx.de>
2025-01-25 20:58:47 +0000

    Add Node fs module to create mock DOM and set up first test function

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// Runs before each test is run
+// Creates mock DOM
+beforeAll(() => {
+    // Add Node fs module, 
+    // file system handling module allowing to open, read & write files
+    let fs = require("fs");
+    // Read content of html file and store in variable
+    // Tests are run from Node root directory, file path starts there
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+// Test if score key exists
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+});
+
